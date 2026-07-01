fe3ae11696bc4c0bb5ea8196d833aa2028dcc98f
prachi-bhadana <bhadanaprachi5@gmail.com>
2026-07-01 18:45:32 +0530

    Use Render environment variables

--- backend/aws_config.py
+++ backend/aws_config.py
@@ -1,3 +1,5 @@
-AWS_ACCESS_KEY = ""
-AWS_SECRET_KEY = ""
-AWS_REGION = "us-east-1"
+import os
+
+AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_REGION = os.getenv("AWS_REGION")
